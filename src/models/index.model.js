1f6632fba5ac8dae1effb67823aef8753110c636
Alvaro Ojeda <286795817+alvaroojedaweb@users.noreply.github.com>
2026-08-22 14:51:50 -0300

    fix: Se comentaron las relaciones entre CLIENTE y CARRITO porque el modelo CLIENTE no esta definido aun y genera un erro de sintaxis

--- src/models/index.model.js
+++ src/models/index.model.js
@@ -5,23 +5,22 @@ import PRODUCTO from './productos.model.js';
 import EMPLEADO from './empleados.model.js';
 import IMAGEN_PRODUCTO from './imagenProducto.model.js';
 import ITEM_CARRITO from './itemCarrito.model.js';
-import CLIENTE from './cliente.model.js';
 
 // Relaciones
-MARCA.hasMany(MODELO,{ foreignKey: "marcaId" })
-MODELO.belongsTo(MARCA,{ foreignKey: "marcaId" })
-MODELO.hasMany(PRODUCTO,{ foreignKey: "modeloId" })
-PRODUCTO.belongsTo(MODELO,{ foreignKey: "modeloId" })
+MARCA.hasMany(MODELO, { foreignKey: "marcaId" })
+MODELO.belongsTo(MARCA, { foreignKey: "marcaId" })
+MODELO.hasMany(PRODUCTO, { foreignKey: "modeloId" })
+PRODUCTO.belongsTo(MODELO, { foreignKey: "modeloId" })
 PRODUCTO.hasMany(IMAGEN_PRODUCTO, { foreignKey: 'ProductoID', as: 'imagenes' });
 IMAGEN_PRODUCTO.belongsTo(PRODUCTO, { foreignKey: 'ProductoID', as: 'producto' });
-CLIENTE.hasMany(ITEM_CARRITO, { foreignKey: 'ClienteID' });
-ITEM_CARRITO.belongsTo(CLIENTE, { foreignKey: 'ClienteID' });
+//CLIENTE.hasMany(ITEM_CARRITO, { foreignKey: 'ClienteID' });
+//ITEM_CARRITO.belongsTo(CLIENTE, { foreignKey: 'ClienteID' });
 PRODUCTO.hasMany(ITEM_CARRITO, { foreignKey: 'ProductoID' });
 ITEM_CARRITO.belongsTo(PRODUCTO, { foreignKey: 'ProductoID' });
 
 // Objeto con todos los modelos para acceso unificado
-const db = { sequelize, MODELO, MARCA, PRODUCTO, EMPLEADO, IMAGEN_PRODUCTO, CLIENTE, ITEM_CARRITO};
+const db = { sequelize, MODELO, MARCA, PRODUCTO, EMPLEADO, IMAGEN_PRODUCTO, ITEM_CARRITO };
 
 // Configurar asociaciones
-export { sequelize, MODELO, MARCA, PRODUCTO, EMPLEADO, IMAGEN_PRODUCTO, ITEM_CARRITO, CLIENTE };
+export { sequelize, MODELO, MARCA, PRODUCTO, EMPLEADO, IMAGEN_PRODUCTO, ITEM_CARRITO };
 export default db;
