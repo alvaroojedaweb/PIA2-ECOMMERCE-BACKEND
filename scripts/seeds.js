61ae266227e7f6fd077cef24d677ac4dcbcc093a
Alvaro Ojeda <286795817+alvaroojedaweb@users.noreply.github.com>
2026-08-22 14:21:22 -0300

    actualizar seeder para cargar marcas, modelos y productos por defecto para hacer pruebas

--- scripts/seeds.js
+++ scripts/seeds.js
@@ -1,86 +1,123 @@
 import db from "../src/models/index.model.js";
-const { sequelize, MARCA, PRODUCTO } = db
+const { sequelize, MODELO, MARCA, PRODUCTO } = db
 
 // Datos de ejemplo para la tabla productos.
 const productosSeed = [
-    {
-        nombre: "Samsung Galaxy A50",
-        marca: "Samsung",
-        modelo: "A50",
-        descripcion: "Smartphone Samsung Galaxy A50 con 250Gb de almacenamiento",
-        categoria: "Celulares",
-        precio: 1499999.00,
-        almacenamientoGb: 250,
-        stock: 12,
-        pesoG: 370
-    },
-    // --- 3 Celulares Adicionales ---
-    {
-        nombre: "iPhone 15 Pro",
-        marca: "Apple",
-        modelo: "15 Pro",
-        descripcion: "Smartphone Apple iPhone 15 Pro de 128Gb con chip A17 Pro y acabado en titanio",
-        categoria: "Celulares",
-        precio: 2199999.00,
-        almacenamientoGb: 128,
-        stock: 8,
-        pesoG: 187
-    },
-    {
-        nombre: "Motorola Edge 40 Neo",
-        marca: "Motorola",
-        modelo: "Edge 40 Neo",
-        descripcion: "Smartphone Motorola Edge 40 Neo con 256Gb de almacenamiento y pantalla pOLED curva",
-        categoria: "Celulares",
-        precio: 899999.00,
-        almacenamientoGb: 256,
-        stock: 15,
-        pesoG: 172
-    },
-    {
-        nombre: "Xiaomi Redmi Note 13 Pro",
-        marca: "Xiaomi",
-        modelo: "Redmi Note 13 Pro",
-        descripcion: "Smartphone Xiaomi Redmi Note 13 Pro con cámara de 200MP y 256Gb de memoria",
-        categoria: "Celulares",
-        precio: 749999.00,
-        almacenamientoGb: 256,
-        stock: 20,
-        pesoG: 188
-    },
-    // --- 2 Accesorios ---
-    {
-        nombre: "Cargador Carga Rápida 25W Type-C",
-        marca: "Samsung",
-        modelo: "EP-TA800",
-        descripcion: "Cargador de pared Super Fast Charging 25W con conector USB Tipo C",
-        categoria: "Accesorios",
-        precio: 34999.00,
-        almacenamientoGb: null,
-        stock: 50,
-        pesoG: 63
-    },
-    {
-        nombre: "Funda Protectora Transparente MagSafe",
-        marca: "Apple",
-        modelo: "iPhone 15 Pro Case",
-        descripcion: "Funda de silicona transparente con compatibilidad y alineación magnética MagSafe",
-        categoria: "Accesorios",
-        precio: 49999.00,
-        almacenamientoGb: null,
-        stock: 35,
-        pesoG: 40
-    }
+
 ];
 
-const marcasSeed = [
-    {
-        nombre: "Samsung",
-    },
-    {
-        nombre: "Xiaomi",
+
+
+async function cargarMarcas() {
+    const marcas = [
+        {
+            nombre: "Samsung",
+        },
+        {
+            nombre: "Xiaomi",
+        },
+        {
+            nombre: "Apple",
+        },
+        {
+            nombre: "Motorola",
+        },
+        {
+            nombre: "TecnoSpark",
+        },
+    ];
+
+    for (const item of marcas) {
+        const [marca] = await MARCA.findOrCreate({
+            where: {
+                nombre: item.nombre
+            },
+            defaults: item
+        });
+        console.log(`Marca creada: ${marca.nombre}`);
+
     }
-];
+}
+
+async function cargarModelos() {
+    const modelos = [
+        {
+            nombre: "A50",
+            marcaId: "1"
+        },
+        {
+            nombre: "A52",
+            marcaId: "1"
+        },
+        {
+            nombre: "17 Pro Max",
+            marcaId: "3"
+        },
+        {
+            nombre: "USB-C a Lightning",
+            marcaId: "3"
+        },
+    ];
+
+    for (const item of modelos) {
+        const [modelo] = await MODELO.findOrCreate({
+            where: {
+                nombre: item.nombre
+            },
+            defaults: item
+        });
+        console.log(`Modelo creado: ${modelo.nombre}`);
+
+    }
+}
+
+async function cargarProductos() {
+    const productos = [
+        {
+            nombre: "Samsung Galaxy A50",
+            modeloId: 1,
+            descripcion: "Smartphone Samsung Galaxy A50 con 250Gb de almacenamiento",
+            categoria: "Celulares",
+            precio: 1499999.00,
+            almacenamientoGb: 250,
+            stock: 12,
+            pesoG: 370,
+        },
+        {
+            nombre: "Iphone 17 Pro Max",
+            modeloId: 3,
+            descripcion: "Smartphone Iphone 17 Pro Max ",
+            categoria: "Celulares",
+            precio: 1499999.00,
+            almacenamientoGb: 250,
+            stock: 12,
+            pesoG: 370,
+        }
+        ,
+        {
+            nombre: "Cable Lightning a usb C (2m)",
+            modeloId: 4,
+            descripcion: "Cable Apple Lightning a usb C de dos metros de largo",
+            categoria: "Accesorios",
+            precio: 1499999.00,
+            almacenamientoGb: null,
+            stock: 12,
+            pesoG: 370,
+        }
+    ];
+
+
+    for (const item of productos) {
+        const [producto] = await PRODUCTO.findOrCreate({
+            where: {
+                nombre: item.nombre
+            },
+            defaults: item
+        });
+        console.log(`Producto creado: ${producto.nombre}`);
+
+    }
+}
 
 const runSeed = async () => {
     try {
@@ -91,23 +128,9 @@ const runSeed = async () => {
         await sequelize.sync({ force: true }); //true solo en desarrollo
         console.log('✅ Base de datos sincronizada');
 
-        // Insertamos los productos de ejemplo.
-        for (const item of productosSeed) {
-            const [producto, created] = await PRODUCTO.findOrCreate({
-                where: { nombre: item.nombre },
-                defaults: item,
-            });
-            console.log(created ? `Producto creado: ${producto.nombre}` : `Producto ya existe: ${producto.nombre}`);
-        }
-
-        // Insertamos las marcas de ejemplo.
-        for (const item of marcasSeed) {
-            const [marca, created] = await MARCA.findOrCreate({
-                where: { nombre: item.nombre },
-                defaults: item,
-            });
-            console.log(created ? `Marca creada: ${marca.nombre}` : `Marca ya existe: ${marca.nombre}`);
-        }
+        await cargarMarcas()
+        await cargarModelos()
+        await cargarProductos()
 
         console.log('Seed completado correctamente.');
     } catch (error) {
