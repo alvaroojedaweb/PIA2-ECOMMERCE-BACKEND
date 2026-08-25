9fcc47bfed7ca03e2ddfcd99e159546848a8b255
ivan <aleinllano@gmail.com>
2026-08-25 20:13:11 -0300

    feat: agregar rutas auth register y login segun consigna actualizada

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,7 +1,11 @@
 import { Router } from 'express';
 import { loginCliente, loginAdmin } from '../controllers/authController.js';
+import { crear } from '../controllers/clientes.Controllers.js';
 
 const router = Router();
+
+router.post('/register', crear);
+router.post('/login', loginCliente);
 
 // POST /auth/cliente/login -> Login para clientes
 router.post('/cliente/login', loginCliente);
