7b7ccae9943b67400bf420c7d2adc8db7c98a681
Alvaro Ojeda <286795817+alvaroojedaweb@users.noreply.github.com>
2026-08-24 21:14:54 -0300

    asociar modelo Cliente al index.model.js

--- src/models/index.model.js
+++ src/models/index.model.js
@@ -5,8 +5,7 @@ import PRODUCTO from './productos.model.js';
 import EMPLEADO from './empleados.model.js';
 import IMAGEN_PRODUCTO from './imagenProducto.model.js';
 import ITEM_CARRITO from './itemCarrito.model.js';
-
-//import CLIENTE from './cliente.model.js';
+import Cliente from './clientes.model.js';
 
 
 // Relaciones
@@ -25,8 +24,8 @@ IMAGEN_PRODUCTO.belongsTo(PRODUCTO, { foreignKey: 'ProductoID', as: 'producto' }
 //CLIENTE.hasMany(ITEM_CARRITO, { foreignKey: 'ClienteID' });
 //ITEM_CARRITO.belongsTo(CLIENTE, { foreignKey: 'ClienteID' });
 
-PRODUCTO.hasMany(ITEM_CARRITO, { foreignKey: 'ProductoID' });
-ITEM_CARRITO.belongsTo(PRODUCTO, { foreignKey: 'ProductoID' });
+//PRODUCTO.hasMany(ITEM_CARRITO, { foreignKey: 'ProductoID' });
+//ITEM_CARRITO.belongsTo(PRODUCTO, { foreignKey: 'ProductoID' });
 
 // Objeto con todos los modelos para acceso unificado
 const db = {
@@ -36,7 +35,7 @@ const db = {
   PRODUCTO,
   EMPLEADO,
   IMAGEN_PRODUCTO,
-  //CLIENTE,
+  Cliente,
   ITEM_CARRITO
 };
 
