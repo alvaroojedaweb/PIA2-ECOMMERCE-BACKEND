4c8b044c714a5c74b1455c95e5e2db69ceac91fc
ivan <aleinllano@gmail.com>
2026-09-24 00:20:35 -0300

    feat(auth): POST /auth/login y GET /auth/me para clientes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,14 @@
 import { Router } from 'express';
-import { loginCliente, loginAdmin } from '../controllers/authController.js';
+import { loginCliente, loginAdmin,  login, me  } from '../controllers/authController.js';
 import { crear } from '../controllers/cliente.controllers.js';
+import { verificarCliente } from '../middleware/auth.js';
 
 const router = Router();
 
 router.post('/register', crear);
+
+router.post('/login', login);                   
+router.get('/me', verificarCliente, me);         
 
 router.post('/cliente/login', loginCliente);
 
